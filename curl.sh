ea814d92268090938756989792648bd308d7cb00
mississppi <msspstudio@gmail.com>
2021-08-15 15:31:25 +0900

    add type

--- curl.sh
+++ curl.sh
@@ -2,31 +2,31 @@
 
 GET_URL="xxxxx"
 CLIENT=xxxxx
+POSTTYPE=('one' 'two' 'three')
 OUTPUTFILENAME=${CLIENT}_response.json
 loopcount=5
-totaltime=0
-avaragetime=0
-# for i in {1..3}
-# do
-#     for j in {1..2}
-#     do
-#         echo ${j}
-#     done
-# done
 
-for i in {1..5}
+for type in ${POSTTYPE[@]}
 do
+    echo ${type}
+    totaltime=0
+    avaragetime=0
+    for j in {1..5}
+    do
+        # リクエスト レスポンスタイムだけ取り出す
+        # -s 進捗ださない -o ボディ捨てる -w 時刻 -H 'Cache-Control: no-cache' キャッシュ無効
+        # res=`curl -H 'Cache-Control: no-cache' ${GET_URL} -s -o /dev/null -w  "%{time_starttransfer}\n"`
+        # totaltime=`bc -l <<< "$totaltime+$res"`
 
-    # リクエスト レスポンスタイムだけ取り出す
-    # -s 進捗ださない -o ボディ捨てる -w 時刻 -H 'Cache-Control: no-cache' キャッシュ無効
-    res=`curl -H 'Cache-Control: no-cache' ${GET_URL} -s -o /dev/null -w  "%{time_starttransfer}\n"`
-    totaltime=`bc -l <<< "$totaltime+$res"`
+        # # sleepさせる
+        sleep 1
 
-    # sleepさせる
-    sleep 1
-
+        #最後のみレスポンスを保存
+        if [ ${j} = 5 ] ; then
+            # data=`curl -H 'Cache-Control: no-cache' ${GET_URL} -s -o ${OUTPUTFILENAME}`
+        fi
+    done
+    #平均計算 bc -l で計算
+    # avaragetime=`bc -l <<< "$totaltime/5"`
+    # echo ${avaragetime}
 done
-avaragetime=`bc -l <<< "$totaltime/5"`
-echo ${avaragetime}
-
-data=`curl -H 'Cache-Control: no-cache' ${GET_URL} -s -o ${OUTPUTFILENAME}`
